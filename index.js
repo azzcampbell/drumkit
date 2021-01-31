6c27fda4fa30eaaae041a0da2ea3aef0cf08ffc7
Gurkage <azz.campbell@gmail.com>
2021-01-31 22:28:48 +0000

    Keyboard and mouse click implementation

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 var elements = document.querySelectorAll(".drum")
 
 for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", function() {
-    alert("I got clicked!")
-  })
+  elements[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML
+    playSound(buttonInnerHTML)
+   })
+}
+
+document.addEventListener("keydown", function() {
+   playSound(event.key)
+})
+
+
+function playSound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio(`sounds/tom-1.mp3`)
+      tom1.play()
+    break;
+
+    case "a":
+      var tom2 = new Audio(`sounds/tom-2.mp3`)
+      tom2.play()
+    break;
+
+    case "s":
+      var tom3 = new Audio(`sounds/tom-3.mp3`)
+      tom3.play()
+    break;
+
+    case "d":
+      var tom4 = new Audio(`sounds/tom-4.mp3`)
+      tom4.play()
+    break;
+
+    case "j":
+      var bass = new Audio(`sounds/kick-bass.mp3`)
+      bass.play()
+    break;
+
+    case "k":
+      var snare = new Audio(`sounds/snare.mp3`)
+      snare.play()
+    break;
+
+    case "l":
+      var crash = new Audio(`sounds/crash.mp3`)
+      crash.play()
+    break;
+
+    default:
+    console.log(key)
+
+  }
+
 }
